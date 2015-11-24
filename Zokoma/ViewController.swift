940678c49cf48f03b94e8f4bea3f1740cf41a520
Jiro Lin <jiro.lin9611@gmail.com>
2015-11-24 13:44:01 +0800

    Initial Commit

---
+++ Zokoma/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Zokoma
+//
+//  Created by jiro9611 on 11/24/15.
+//  Copyright © 2015 jiro9611. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
